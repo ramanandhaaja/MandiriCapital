1761657cf882bfac59b7207fd80f796f6215aac0
ramanandhaaja <thisisnandha@gmail.com>
2024-11-17 21:22:01 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
-Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 Route::get('/portfolio', [PageController::class, 'portfolio'])->name('portfolio');
 Route::get('/event', [PageController::class, 'event'])->name('event');
 Route::get('/media', [PageController::class, 'media'])->name('media');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
+
 
 /*
 <!-- Using route names -->
@@ -19,3 +21,5 @@ Route::get('/media', [PageController::class, 'media'])->name('media');
 <a href="{{ url('/') }}">Home</a>
 <a href="{{ url('/about') }}">About</a>
 */
+
+
