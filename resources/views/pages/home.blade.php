924e63791439099bffa2f1c177defd4e685a75dc
ramanandhaaja <thisisnandha@gmail.com>
2024-11-18 00:07:00 +0700

    commit main

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,8 +2,16 @@
 @section("container")
 
 <div class="hero-section">
-    <h1>Investing in tomorrow's leaders</h1>
-    <a href="#" class="cta-button">Get Investment</a>
+    <h1 class="center-text" >Investing in tomorrow's leaders</h1>
+    <img src="{{ asset('images/home/Ellipse110.png') }}" class="bg-left-1">
+    <img src="{{ asset('images/home/Ellipse111.png') }}" class="bg-left-2">
+    <img src="{{ asset('images/home/Ellipse112.png') }}" class="bg-left-3">
+
+
+    <a href="#" >
+        <img src="{{ asset('images/home/getInvestment.png') }}" alt="Get Investment" class="button-image center-image">
+    </a>
+
 </div>
 
 <div class="grid-container">
@@ -48,4 +56,3 @@
     </div>
 </div>
 
-@endsection
