6c4650ccded026275612da586312355fd23c3e5b
ramanandhaaja <thisisnandha@gmail.com>
2024-11-24 22:00:57 +0700

    commit

---
+++ resources/views/pages/report.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.mainlayout')
+
+@section('localcss')
+    <link href="{{ asset('css/report.css') }}" rel="stylesheet">
+@endsection
+
+@section('container')
+    <div class="hero-section">
+    </div>
+
+    {{-- Report Content Section --}}
+    <div class="report-content-section">
+    </div>
+@endsection
