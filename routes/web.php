0ab33c7a83cb69bcbb2df479b4940315fb904695
ramanandhaaja <thisisnandha@gmail.com>
2024-11-29 12:41:00 +0700

    update breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
@@ -14,18 +29,6 @@ Route::get('/media/filter/{tag}', [PageController::class, 'mediaFilter'])->name(
 Route::get('/report', [PageController::class, 'report'])->name('report');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
-// routes/web.php
-use App\Http\Controllers\BlogController;
-
 Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
 Route::get('/blog/category/{slug}', [BlogController::class, 'byCategory'])->name('blog.category');
 Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
-/*
-<!-- Using route names -->
-<a href="{{ route('home') }}">Home</a>
-<a href="{{ route('about') }}">About</a>
-<a href="{{ route('contact') }}">Contact</a>
-
-<a href="{{ url('/') }}">Home</a>
-<a href="{{ url('/about') }}">About</a>
-*/
