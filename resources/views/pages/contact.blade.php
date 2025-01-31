f2a470df73cb69f0794ee187ed14b52144f98218
ramanandhaaja <thisisnandha@gmail.com>
2025-01-31 13:50:18 +0700

    update contact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -202,24 +202,23 @@
     {{-- Map Section --}}
     <section class="map-section">
         <div class="map-container">
-            <div class="map-grid">
-                <div>
-                    <img src="{{ asset('images/contact/map.png') }}" alt="Office Location Map" class="map-iframe">
-                </div>
 
-                <div class="map-content">
-                    <h2 class="map-title">
-                        Visit Our<br>
-                        Head Quarter
-                    </h2>
 
-                    <div class="map-info">
-                        <h3 class="company-name">MANDIRI CAPITAL INDONESIA</h3>
-                        <p class="address">
-                            Menara Mandiri II, lantai 14,<br>
-                            Jl. Jend. Sudirman No. 54-55, Jakarta 12190
-                        </p>
-                    </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.297882683585!2d106.8052263107502!3d-6.224397960934856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1caf7c75bad%3A0xe6744fb7fef268fd!2sPT.%20Mandiri%20Capital%20Indonesia!5e0!3m2!1sen!2sid!4v1738305411568!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="map-grid">
+            <div class="map-content">
+                <h2 class="map-title">
+                    Visit Our<br>
+                    Head Quarter
+                </h2>
+
+                <div class="map-info">
+                    <h3 class="company-name">MANDIRI CAPITAL INDONESIA</h3>
+                    <p class="address">
+                        Menara Mandiri II, lantai 14,<br>
+                        Jl. Jend. Sudirman No. 54-55, Jakarta 12190
+                    </p>
                 </div>
             </div>
         </div>
